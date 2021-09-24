15647bbcf8f1c0a18c0e8e6d16fcbc79a0f7f68c
Elfrey Jude Ochavo <elfrey.ochavo@dawsoncollege.qc.ca>
2021-09-24 11:42:19 -0400

    Step 3.2

---
+++ surveyresult.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>surveyresult.php</title>
+    </head>
+    <body>
+        <p><?= var_dump($_GET) ?></p>
+        <p><?= var_dump($_POST) ?></p>
+    </body>
+</html>
