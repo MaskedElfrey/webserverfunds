c6d5fd39f35d0bd5ebbf570e85ba2ff8a09868af
Elfrey Jude Ochavo <elfrey.ochavo@dawsoncollege.qc.ca>
2021-09-24 12:36:36 -0400

    3.5 test

--- surveyresult.php
+++ surveyresult.php
@@ -4,10 +4,43 @@
         <title>surveyresult.php</title>
     </head>
     <body>
-        <p><?= var_dump($_GET) ?></p>
-        <p><?= var_dump($_POST) ?></p>
+        <div>
+            <p>Hi <?= htmlspecialchars($_POST['name'])?>.</p>
+            <p>You chose <?= htmlspecialchars($_POST['play_ball'])?> to playing basketball.</p>
+            <p>frequency: <?= htmlspecialchars($_POST['time'])?></p> 
+            <p>Your shoe preference(s):<br>
+                <?php
+                    if(isset($_POST['brand1'])
+                    {
+                        echo "Nike<br/>";
+                    }   
 
-        <div>Hi <?= htmlspecialchars($_POST['name'])?>
+                    if(isset($_POST['brand2'])
+                    {
+                        echo "Adidas<br/>";
+                    } 
+
+                    if(isset($_POST['brand3'])
+                    {
+                        echo "Jordan<br/>";
+                    }  
+
+                    if(isset($_POST['brand4'])
+                    {
+                        echo "Puma<br/>";
+                    } 
+
+                    if(isset($_POST['brand5']) 
+                    {
+                        echo "Reebok<br/>";
+                    }  
+                ?>
+            </p>
+            <p>Your favorite player is <?= htmlspecialchars($_POST['player'])?>. </p>
+            <p>Your opinion about Basketball: <br>
+                <?= htmlspecialchars($_POST['opinion'])?>
+            </p>
+            
         </div>
     </body>
 </html>
