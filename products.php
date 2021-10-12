3d49e819b093983fdf5cf713f03113c04ee25097
Elfrey Jude Ochavo <elfrey.ochavo@dawsoncollege.qc.ca>
2021-10-12 10:50:33 -0400

    Midterm

---
+++ products.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>products.php</title>
+    </head>
+    <body>
+        <div>
+            <p>You have requested <?= htmlspecialchars($_POST['quantity'])?> <?= htmlspecialchars($_POST['product'])?></p>
+            </p>  
+        </div>
+    </body>
+</html>
