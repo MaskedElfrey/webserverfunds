07d43913e2ae0a92b6bd7b03867d8b4f8ccff21c
Elfrey Jude Ochavo <elfrey.ochavo@dawsoncollege.qc.ca>
2021-11-19 12:17:36 -0500

    Lab 11

---
+++ weather.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>Your shoe preference(s):<br>
+            <?php
+                $raw = `./bme280`;
+                $deserialized = json_decode($raw);
+            
+                if(isset($_POST['var1']))
+                {
+                    echo "Temperature: {$deserialized->temperature}<br/>";
+                }   
+
+                if(isset($_POST['var2']))
+                {
+                    echo "Pressure: {$deserialized->pressure}<br/>";
+                } 
+
+                if(isset($_POST['var3']))
+                {
+                    echo "Altitude: {$deserialized->altitude}<br/>";
+                }   
+            ?>
+        </p>
+    </body>
+</html>
