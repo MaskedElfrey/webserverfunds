e49ad76cbb08e3d0898872965b5bc6bd6cb9579b
Elfrey Jude Ochavo <elfrey.ochavo@dawsoncollege.qc.ca>
2021-12-21 11:45:05 -0500

    Update g00gle.php

--- g00gle.php
+++ g00gle.php
@@ -2,7 +2,23 @@
 <html>
     <body>
         <?= htmlspecialchars($_POST['search'])?>
-    
-        <?= superglobal $_SERVER['REMOTE_ADDR']?>
+        <?php
+            $server = "localhost";
+            $username = "maskedelfrey";
+            $password = "Bloodtatoo108";
+            $database = "g00gle";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+            $sql = "select * from ip_add;";
+            $result = mysqli_query($conn, $sql);
+
+            $ip = $_SERVER['REMOTE_ADDR'];
+            echo $ip;
+        ?>
+    </head>
     </body>
 </html>
